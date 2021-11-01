cd77843339dc1dcae9da48a2627fe3ed9e300054
Alan Keller <52039446+Kelly0210@users.noreply.github.com>
2021-11-01 12:27:03 +0600

    Paginator + deploy

--- src/common/Paginator.jsx
+++ src/common/Paginator.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from '../Components/Users/Users.module.css';
 
 function Paginator(props) {
@@ -9,13 +9,28 @@ function Paginator(props) {
         pages.push(i);
     }
 
+    let portionSize = 10;
+    let portionCount = Math.ceil(pagesCount / portionSize);
+    let [portionNumber, setPortionNumber] = useState(1);
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize;
+
     return <div>
-        {pages.map(page => {
-            return <span className={props.currentPage === page && style.pickedPage}
-                         onClick={(event) => {
-                             props.onPageChanged(page)
-                         }}>{page}</span>
-        })}
+        {portionNumber > 1 && <button onClick={() => {
+            setPortionNumber(portionNumber - 1)
+        }}>PREV</button>}
+        {pages
+            .filter(page => page >= leftPortionPageNumber && page <= rightPortionPageNumber)
+            .map((page) => {
+                return <span className={props.currentPage === page && style.pickedPage}
+                             key={page}
+                             onClick={(event) => {
+                                 props.onPageChanged(page)
+                             }}>{page}</span>
+            })}
+        {portionCount > portionNumber && <button onClick={() => {
+            setPortionNumber(portionNumber + 1)
+        }}>NEXT</button>}
     </div>
 }
 
